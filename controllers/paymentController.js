4e90c8f0ea4afb078bf0a39d5c1d27fb66759d34
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-16 13:46:45 +0600

    Created payment and order models and ipn function.

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,12 +1,33 @@
-// const SSLCommerz = require("ssl-commerz-node");
 const SSLCommerz = require("ssl-commerz-node");
 const PaymentSession = SSLCommerz.PaymentSession;
 
 const { CartItem } = require("../models/cartItem");
 const { Profile } = require("../models/profile");
+const { Order } = require("../models/order");
+const { Payment } = require("../models/payment");
+
+//! Request a session
+//! Payment process
+//! Receive IPN
+//! Create an order
 
 module.exports.ipn = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
+    const payment = new Payment(req.body);
+    const tran_id = payment["tran_id"];
+    if (payment["status"] === "VALID ") {
+        const order = await Order.updateOne(
+            { transaction_id: tran_id },
+            { status: "Complete" }
+        );
+        await CartItem.deleteMany(order.cartItems);
+    } else {
+        await Order.deleteOne({ transaction_id: tran_id });
+    }
+
+    await payment.save();
+
+    return res.status(200).send("IPN");
 };
 
 module.exports.initPayment = async (req, res) => {
@@ -86,5 +107,17 @@ module.exports.initPayment = async (req, res) => {
     });
 
     response = await payment.paymentInit();
+
+    let order = new Order({
+        cartItems: cartItems,
+        user: userId,
+        transaction_id: tran_id,
+        address: profile,
+    });
+
+    if (response.status === "SUCCESS") {
+        order.sessionKey = response["sessionkey"];
+        await order.save();
+    }
     return res.status(200).send(response);
 };
