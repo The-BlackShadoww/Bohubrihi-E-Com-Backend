8aa892c2c278c8421a9e070c2e2487c8102a0c87
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-16 17:01:57 +0600

    Add changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-Com-Backend
-## I have deployed this backend app in render.com
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const error = require("./middlewares/error");
+//! This is for testing. 
 //! ---- Middlewares -----
 //* we don't have to write middlewares/index. Because by default it will call the index.js file.
 require("./middlewares")(app);
