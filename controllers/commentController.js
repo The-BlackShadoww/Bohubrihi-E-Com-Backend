cdeafc4da10a71577e2a49149b70332ba1839554
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-19 22:41:23 +0600

    comment 2nd att

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,6 +3,7 @@ const { Comments } = require("../models/comments");
 
 module.exports.postComment = async (req, res) => {
     console.log(req);
+    console.log(req.body);
     const userId = req.user._id;
     // const productId= req
     const userComment = _.pick(req.body, "comment");
