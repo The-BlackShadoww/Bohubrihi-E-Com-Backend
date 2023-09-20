8f6356e5c8a2026f653ee593cec82a348cba1c31
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-20 09:58:30 +0600

    comment try 4.3

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,14 +2,13 @@ const _ = require("lodash");
 const Comments = require("../models/comments");
 
 module.exports.postComment = async (req, res) => {
-    console.log(req.body);
-    const userId = req.user._id;
-    const userComment = _.pick(req.body, ["comment", "productId"]);
-    userComment["user"] = userId;
-
-    let newComment = new Comments(userComment);
-
     try {
+        const userId = req.user._id;
+        const userComment = _.pick(req.body, ["comment", "productId"]);
+        userComment["user"] = userId;
+        console.log("this is userComment: ", userComment);
+
+        let newComment = new Comments(userComment);
         await newComment.save();
         return res.status(200).send("Comment received");
     } catch (err) {
