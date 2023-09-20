ee173c5194d614f11fb5599d887e5ae6c7b8538e
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-20 11:01:51 +0600

    comment over --> dissolved

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -6,11 +6,10 @@ module.exports.postComment = async (req, res) => {
         const userId = req.user._id;
         const userComment = _.pick(req.body, ["comment", "productId"]);
         userComment["user"] = userId;
-        console.log("this is userComment: ", userComment);
 
         let newComment = new Comments(userComment);
         await newComment.save();
-        return res.status(200).send("Comment received");
+        return res.status(200).send("Comment successfully posted");
     } catch (err) {
         console.log(err);
     }
