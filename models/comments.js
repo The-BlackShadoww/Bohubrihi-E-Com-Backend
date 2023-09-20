73e2504dcafcbeefbab3ab2b6184f4f7decbb4b8
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-20 11:23:06 +0600

    comment try 9

--- models/comments.js
+++ models/comments.js
@@ -1,15 +1,34 @@
+// const { Schema, model } = require("mongoose");
+
+// module.exports.Comments = model(
+//     "comments",
+//     Schema({
+//         user: {
+//             type: Schema.Types.ObjectId,
+//             ref: "User",
+//         },
+//         productId: String,
+//         comment: String,
+//     })
+// );
+
+// Comments.index({ user: 1 });
+
+
+//!---------------------------------------
+
 const { Schema, model } = require("mongoose");
 
-module.exports.Comments = model(
-    "comments",
-    Schema({
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        },
-        productId: String,
-        comment: String,
-    })
-);
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    productId: String,
+    comment: String,
+});
 
-Comments.index({ user: 1 });
+// Remove the unique: true option
+commentSchema.index({ user: 1 });
+
+module.exports.Comments = model("Comment", commentSchema);
