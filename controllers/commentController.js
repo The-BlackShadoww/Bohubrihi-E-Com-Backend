953489bc3b5d4d489c2b075b9b38cc8f73394de4
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-22 00:43:57 +0600

    comment check

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 const { Comments } = require("../models/comments");
 
 module.exports.postComment = async (req, res) => {
+    console.log(req.body);
     try {
         const userId = req.user._id;
         const userComment = _.pick(req.body, ["comment", "product"]);
