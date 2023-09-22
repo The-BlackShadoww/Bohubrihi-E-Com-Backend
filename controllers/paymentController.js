46443a123b5639d78e4421276495c2f30ca5e357
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-22 22:27:09 +0600

    Changes made in paymentController and cartItem m.

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -33,7 +33,8 @@ module.exports.ipn = async (req, res) => {
 
 module.exports.initPayment = async (req, res) => {
     const userId = req.user._id;
-    const cartItems = await CartItem.find({ user: userId });
+    //! Changes made !!
+    const cartItems = await CartItem.find({ user: userId }).populate("product");
     //todo profile
     const profile = await Profile.findOne({ user: userId });
     const { address1, address2, city, state, postcode, country, phone } =
@@ -111,7 +112,13 @@ module.exports.initPayment = async (req, res) => {
     response = await payment.paymentInit();
 
     let order = new Order({
-        cartItems: cartItems,
+        cartItems: cartItems.map((item) => ({
+            productId: item.product._id,
+            name: item.product.name,
+            price: item.price,
+            count: item.count,
+            user: item.user,
+        })),
         user: userId,
         transaction_id: tran_id,
         address: profile,
@@ -127,3 +134,135 @@ module.exports.initPayment = async (req, res) => {
 module.exports.paymentSuccess = async (req, res) => {
     res.sendFile(path.join(__basedir + "/public/success.html"));
 };
+
+//todo ========================== original code ============================================
+
+// const SSLCommerz = require("ssl-commerz-node");
+// const PaymentSession = SSLCommerz.PaymentSession;
+
+// const { CartItem } = require("../models/cartItem");
+// const { Profile } = require("../models/profile");
+// const { Order } = require("../models/order");
+// const { Payment } = require("../models/payment");
+// const path = require("path");
+
+// //! Request a session
+// //! Payment process
+// //! Receive IPN
+// //! Create an order
+
+// module.exports.ipn = async (req, res) => {
+//     // console.log(req.body);
+//     const payment = new Payment(req.body);
+//     const tran_id = payment["tran_id"];
+//     if (payment["status"] === "VALID") {
+//         const order = await Order.updateOne(
+//             { transaction_id: tran_id },
+//             { status: "Complete" }
+//         );
+//         await CartItem.deleteMany(order.cartItems);
+//     } else {
+//         await Order.deleteOne({ transaction_id: tran_id });
+//     }
+
+//     await payment.save();
+
+//     return res.status(200).send("IPN");
+// };
+
+// module.exports.initPayment = async (req, res) => {
+//     const userId = req.user._id;
+//     const cartItems = await CartItem.find({ user: userId });
+//     //todo profile
+//     const profile = await Profile.findOne({ user: userId });
+//     const { address1, address2, city, state, postcode, country, phone } =
+//         profile;
+
+//     const total_amount = cartItems
+//         .map((item) => item.count * item.price)
+//         .reduce((a, b) => a + b, 0);
+
+//     const total_item = cartItems
+//         .map((item) => item.count)
+//         .reduce((a, b) => a + b, 0);
+
+//     const tran_id =
+//         "_" + Math.random().toString(36).substr(2, 9) + new Date().getTime();
+
+//     //todo init payment session
+//     const payment = new PaymentSession(
+//         true,
+//         process.env.SSLCOMMERZ_STORE_ID,
+//         process.env.SSLCOMMERZ_STORE_PASSWORD
+//     );
+
+//     //! Set the urls
+//     payment.setUrls({
+//         success:
+//             "https://bohubrihi-e-com-backend-app.onrender.com/api/payment/success",
+//         fail: "yoursite.com/fail",
+//         cancel: "yoursite.com/cancel",
+//         ipn: "https://bohubrihi-e-com-backend-app.onrender.com/api/payment/ipn",
+//     });
+
+//     //! Set order details
+//     payment.setOrderInfo({
+//         total_amount: total_amount,
+//         currency: "BDT",
+//         tran_id: tran_id,
+//         emi_option: 0,
+//     });
+
+//     //! Set customer info
+//     payment.setCusInfo({
+//         name: req.user.name,
+//         email: req.user.email,
+//         add1: address1,
+//         add2: address2,
+//         city: city,
+//         state: state,
+//         postcode: postcode,
+//         country: country,
+//         phone: phone,
+//         fax: phone,
+//     });
+
+//     //! Set shipping info
+//     payment.setShippingInfo({
+//         method: "Courier",
+//         num_item: total_item,
+//         name: req.user.name,
+//         add1: address1,
+//         add2: address2,
+//         city: city,
+//         state: state,
+//         postcode: postcode,
+//         country: country,
+//     });
+
+//     //! Set Product Profile
+//     payment.setProductInfo({
+//         product_name: "Bohubrihi E-com Products",
+//         product_category: "General",
+//         product_profile: "general",
+//     });
+
+//     response = await payment.paymentInit();
+
+//     let order = new Order({
+//         cartItems: cartItems,
+//         user: userId,
+//         transaction_id: tran_id,
+//         address: profile,
+//     });
+
+//     if (response.status === "SUCCESS") {
+//         order.sessionKey = response["sessionkey"];
+//         await order.save();
+//     }
+//     return res.status(200).send(response);
+// };
+
+// module.exports.paymentSuccess = async (req, res) => {
+//     res.sendFile(path.join(__basedir + "/public/success.html"));
+// };
--- models/cartItem.js
+++ models/cartItem.js
@@ -20,12 +20,42 @@ const CartItemSchema = Schema(
             ref: "User",
             required: true,
         },
-        // purchased: {
-        //     // if purchased true, if not then false.
-        // }
+        name: String,
     },
     { timestamps: true }
 );
 
 module.exports.CartItemSchema = CartItemSchema;
 module.exports.CartItem = model("CartItem", CartItemSchema);
+
+//todo ========================== original code ============================================
+
+// const { Schema, model } = require("mongoose");
+// const joi = require("joi");
+
+// const CartItemSchema = Schema(
+//     {
+//         product: {
+//             type: Schema.Types.ObjectId,
+//             ref: "Product",
+//             required: true,
+//         },
+//         price: Number,
+//         count: {
+//             type: Number,
+//             default: 1,
+//             min: 1,
+//             max: 5,
+//         },
+//         user: {
+//             type: Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+
+//     },
+//     { timestamps: true }
+// );
+
+// module.exports.CartItemSchema = CartItemSchema;
+// module.exports.CartItem = model("CartItem", CartItemSchema);
