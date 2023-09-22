c4729d3753da0816266c0c5f379256b9ea203dbc
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-22 23:25:51 +0600

    changes again

--- models/cartItem.js
+++ models/cartItem.js
@@ -1,35 +1,3 @@
-// const { Schema, model } = require("mongoose");
-// const joi = require("joi");
-
-// const CartItemSchema = Schema(
-//     {
-//         product: {
-//             type: Schema.Types.ObjectId,
-//             ref: "Product",
-//             required: true,
-//         },
-//         price: Number,
-//         count: {
-//             type: Number,
-//             default: 1,
-//             min: 1,
-//             max: 5,
-//         },
-//         user: {
-//             type: Schema.Types.ObjectId,
-//             ref: "User",
-//             required: true,
-//         },
-//         name: String,
-//     },
-//     { timestamps: true }
-// );
-
-// module.exports.CartItemSchema = CartItemSchema;
-// module.exports.CartItem = model("CartItem", CartItemSchema);
-
-//todo ========================== original code ============================================
-
 const { Schema, model } = require("mongoose");
 const joi = require("joi");
 
@@ -52,10 +20,42 @@ const CartItemSchema = Schema(
             ref: "User",
             required: true,
         },
-
+        name: String,
     },
     { timestamps: true }
 );
 
 module.exports.CartItemSchema = CartItemSchema;
 module.exports.CartItem = model("CartItem", CartItemSchema);
+
+//todo ========================== original code ============================================
+
+// const { Schema, model } = require("mongoose");
+// const joi = require("joi");
+
+// const CartItemSchema = Schema(
+//     {
+//         product: {
+//             type: Schema.Types.ObjectId,
+//             ref: "Product",
+//             required: true,
+//         },
+//         price: Number,
+//         count: {
+//             type: Number,
+//             default: 1,
+//             min: 1,
+//             max: 5,
+//         },
+//         user: {
+//             type: Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+
+//     },
+//     { timestamps: true }
+// );
+
+// module.exports.CartItemSchema = CartItemSchema;
+// module.exports.CartItem = model("CartItem", CartItemSchema);
