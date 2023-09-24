bbf8aff8fa8598794476e435a9e551cbdd9444ac
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-24 21:42:38 +0600

    try validation 12

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -3,7 +3,7 @@ const {
     initPayment,
     ipn,
     paymentSuccess,
-} = require("../controllers/paymentController");
+} = require("../controllers/newPayment.mjs");
 const authorize = require("../middlewares/authorize");
 
 router.route("/").get(authorize, initPayment);
@@ -11,3 +11,18 @@ router.route("/ipn").post(ipn);
 router.route("/success").post(paymentSuccess);
 
 module.exports = router;
+
+//todo ========== original ===========
+// const router = require("express").Router();
+// const {
+//     initPayment,
+//     ipn,
+//     paymentSuccess,
+// } = require("../controllers/paymentController");
+// const authorize = require("../middlewares/authorize");
+
+// router.route("/").get(authorize, initPayment);
+// router.route("/ipn").post(ipn);
+// router.route("/success").post(paymentSuccess);
+
+// module.exports = router;
